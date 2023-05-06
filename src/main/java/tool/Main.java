bd7ce2af1f7a41256e8c3b37a21f017255a1741c
VladislavPastushenko <vlad1023333@gmail.com>
2023-05-06 12:42:27 +0200

    Work with UI has been started

--- src/main/java/tool/Main.java
+++ src/main/java/tool/Main.java
@@ -10,10 +10,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        Label l = new Label("Works epta");
         Scene scene = new Scene(new StackPane(l), 640, 480);
+
+        stage.setTitle("IJA");
         stage.setScene(scene);
         stage.show();
     }
