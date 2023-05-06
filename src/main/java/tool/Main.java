774fa6ad77901afe966310929b3581aa7bcdb3ca
VladislavPastushenko <vlad1023333@gmail.com>
2023-05-06 14:17:33 +0200

    Some progress

--- src/main/java/tool/Main.java
+++ src/main/java/tool/Main.java
@@ -1,21 +1,77 @@
 package tool;
 
 import javafx.application.Application;
-import javafx.scene.Scene;
+import javafx.scene.*;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
+import javafx.scene.layout.*;
+import javafx.stage.*;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.fxml.FXMLLoader;
+
+
+import java.net.URL;
+
+import tool.common.*;
+import tool.game.*;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage stage) {
-        Label l = new Label("Works epta");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
+    public void start(Stage stage) throws Exception {
+        MazeConfigure cfg = new MazeConfigure();
+        cfg.startReading(4, 3);
+        cfg.processLine("..G");
+        cfg.processLine(".X.");
+        cfg.processLine(".X.");
+        cfg.processLine(".S.");
+        cfg.stopReading();
 
-        stage.setTitle("IJA");
-        stage.setScene(scene);
+        CommonMaze maze = cfg.createMaze();
+
+        VBox vbox = new VBox();
+        vbox.setAlignment(Pos.CENTER);
+
+        for (int row = 0; row < maze.numRows(); row++) {
+
+            HBox hbox = new HBox();
+            hbox.setAlignment(Pos.CENTER);
+
+            for (int col = 0; col < maze.numCols(); col++) {
+                CommonField field = maze.getField(row, col);
+
+                Label fieldLabel;
+
+                if (field.canMove()) {
+                    if (field.isEmpty()) {
+                        fieldLabel = new Label("_");
+                    }
+                    else {
+                        CommonMazeObject obj = field.get();
+                        if (obj.isPacman()) {
+                            fieldLabel = new Label("P");
+                        } else {
+                            fieldLabel = new Label("G");
+                        }
+                    }
+                }
+                else {
+                    fieldLabel = new Label("X");
+                }
+                hbox.getChildren().add(fieldLabel);
+            }
+            vbox.getChildren().add(hbox);
+        }
+
+        stage.setTitle("IJA Pacman");
+        stage.setWidth(600);
+        stage.setHeight(600);
+
+        Scene primaryScene = new Scene(vbox);
+        stage.setScene(primaryScene);
+
         stage.show();
+
     }
 
     public static void main(String[] args) {
